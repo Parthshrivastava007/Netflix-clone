e4d0c7a4505c4165a2b27946f1d900c85247bae7
Parthshrivastava007 <parth,dollor@gmail.com>
2025-06-06 17:06:21 +0530

    feat: implement toggle functionality for sign in and sign up forms in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,14 @@
 import Header from "./Header";
 import { LOGIN_URL } from "../utlis/Images";
+import { useState } from "react";
 
 const Login = () => {
+  const [isSigninForm, setisSigninForm] = useState(true);
+
+  const toggleSigninForm = () => {
+    setisSigninForm(!isSigninForm);
+  };
+
   return (
     <div>
       <Header />
@@ -9,7 +16,9 @@ const Login = () => {
         <img src={LOGIN_URL} alt="Bg-image" />
       </div>
       <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSigninForm ? "Sign in" : "Sign up"}
+        </h1>
         <input
           type="text"
           placeholder="Email Address"
@@ -20,7 +29,12 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
-        <button className="p-4 my-6 bg-red-700 w-full">Sign In</button>
+        <button className="p-4 my-6 bg-red-700 w-full">
+          {isSigninForm ? "Sign in" : "Sign up"}
+        </button>
+        <p className="py-4" onClick={toggleSigninForm}>
+          {isSigninForm ? "New to Netflix? Sign up Now" : "Already Registered? Sign in Now"}
+        </p>
       </form>
     </div>
   );
