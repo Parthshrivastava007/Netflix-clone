cd316613ba45b2e700b2975701526735ee4963a7
Parthshrivastava007 <parth,dollor@gmail.com>
2025-06-06 16:57:59 +0530

    feat: refactor Header and Login components to use constants for image URLs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,27 @@
 import Header from "./Header";
+import { LOGIN_URL } from "../utlis/Images";
 
 const Login = () => {
   return (
     <div>
       <Header />
-      <div>
-        <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/6863f6e8-d419-414d-b5b9-7ef657e67ce4/web/IN-en-20250602-TRIFECTA-perspective_27a3fdfa-126f-4148-b153-55d60b51be6a_small.jpg"
-          alt="Bg-image"
-        />
+      <div className="absolute">
+        <img src={LOGIN_URL} alt="Bg-image" />
       </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full">Sign In</button>
+      </form>
     </div>
   );
 };
