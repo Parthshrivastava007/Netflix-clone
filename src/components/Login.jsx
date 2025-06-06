e4b1ab11290670f279d1ad74bd9382bef654e781
Parthshrivastava007 <parth,dollor@gmail.com>
2025-06-06 14:33:49 +0530

    feat: update Header and Login components for layout and logo integration

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,17 @@
 import Header from "./Header";
 
 const Login = () => {
-  return <div>
-    <Header/>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/6863f6e8-d419-414d-b5b9-7ef657e67ce4/web/IN-en-20250602-TRIFECTA-perspective_27a3fdfa-126f-4148-b153-55d60b51be6a_small.jpg"
+          alt="Bg-image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
