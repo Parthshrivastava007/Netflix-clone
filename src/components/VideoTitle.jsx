167e4c4f277ae4199ad44f134999c538b5df9a62
Parthshrivastava007 <parth,dollor@gmail.com>
2025-06-11 15:37:31 +0530

    feat: enhance MovieList and VideoTitle components; add scrollbar-hide class for improved styling

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -4,7 +4,7 @@ const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-4xl font-bold">{title}</h1>
-      <p className="py-6 text-lg w-1/3">{overview}</p>
+      <p className="py-6 w-1/3">{overview}</p>
       <div className="flex">
         <button className="flex gap-2 bg-white text-black p-4 px-12 text-xl rounded-lg cursor-pointer hover:opacity-80">
           <img className="h-7 w-7" src={PLAY_BUTTON} />
